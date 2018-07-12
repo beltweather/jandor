cc539b1c4fc98f0da6c858c0f0095f3d34813393
jmharter <40039714+jmharter@users.noreply.github.com>
2018-07-12 10:27:22 -0500

    Updated Jandor version
    Now calling this Jandor version 2.1

--- Jandor/src/util/VersionUtil.java
+++ Jandor/src/util/VersionUtil.java
@@ -7,7 +7,7 @@ import session.User;
 
 public class VersionUtil {
 	
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "2.1";
 	
 	public static String MTG_JSON_VERSION = "3.18";
 
